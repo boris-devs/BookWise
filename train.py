50d400ec2fb33f63c889b725be3061d69b43a97a
xborismenx <boris.tikhonov.21@gmail.com>
2026-01-21 15:44:34 +0200

    Add train.py with run_training_model function to train the genre classifier

---
+++ train.py
@@ -0,0 +1,25 @@
+from classifier import GenreClassifier
+from settings import MODEL_PATH
+from data_collector import book_cl
+
+
+def run_training_model():
+    if any(MODEL_PATH.iterdir()):
+        print(f"Model already exists in {MODEL_PATH}! To re-train, delete this folder or its content.")
+        return
+
+    print("Starting training...")
+    common_genres = book_cl.most_common_genres()
+    genres_classifier = GenreClassifier(num_genres=len(common_genres))
+    
+    genres_classifier.train(
+        book_cl.all_descriptions(),
+        common_genres,
+        book_cl.all_genres()
+    )
+    print("Training completed successfully.")
+
+
+if __name__ == "__main__":
+    run_training_model()
+    
